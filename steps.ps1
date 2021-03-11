cd10d35d46d1ceda5e20aae0b4792bf24015448e
Niels Swimberghe <nielsswimberghe@gmail.com>
2021-03-10 22:31:31 -0500

    add more steps

--- steps.ps1
+++ steps.ps1
@@ -8,3 +8,12 @@ git add *
 git commit -m "Add Blazor Wasm project"
 gh repo create
 git push --set-upstream origin main
+
+# create dockerfile.build
+
+git add --all
+git commit -m "Add dockerfile.build"
+git push
+
+doctl auth init
+doctl app create --spec .do/app.yaml
